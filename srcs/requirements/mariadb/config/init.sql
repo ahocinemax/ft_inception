f9eff1ac88c75db7b91e7a44f36658cd726ec97a
ahocine <ahocine@student.42.fr>
2023-03-01 17:44:15 +0100

    init.sql removed, included directly on Dockerfile

--- srcs/requirements/mariadb/config/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS inceptiondb;
-CREATE USER IF NOT EXISTS 'ahocine'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON inceptiondb.* TO 'ahocine'@'%';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED by 'password';
