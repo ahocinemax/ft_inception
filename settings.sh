5bdee9bb06dcef804c9e880b38a23f59c0c0dc83
ahocine <ahocine@student.42.fr>
2023-03-01 03:23:00 +0100

    MDP for ROOT in mariadb

--- settings.sh
+++ settings.sh
@@ -3,9 +3,11 @@
 # # Allow members of group sudo to execute any command
 # echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-# sudo apt-get install build-essential -y docker.io git curl
+# sudo apt-get install build-essential docker.io git curl -y
 
 # DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 # mkdir -p $DOCKER_CONFIG/cli-plugins
 # curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 # chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+# docker exec -it mysql -u ahocine -ppassword -e "CONNECT inceptiondb; SHOW TABLES;"
